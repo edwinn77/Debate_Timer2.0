c8eeaca10643a0a0a05866b982172dc8e8933916
Edwin <edwinchia0710@gmail.com>
2026-02-14 01:21:12 +0800

    Add 1v1, 2v2, 3v3 templates & configure GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // Important for GitHub Pages to load assets correctly
+  base: '/Debate_Timer/', // Must match GitHub repo name for GitHub Pages
 })
